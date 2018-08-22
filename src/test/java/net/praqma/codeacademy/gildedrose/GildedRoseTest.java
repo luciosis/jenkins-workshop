f8e9b67cbfecbe7c5f48712332ff2c93c597b9c9
luciosis <marc.henriksen90@gmail.com>
2018-08-22 07:50:12 +0000

    This commit is a test

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-
+***
+New test 
+***
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
