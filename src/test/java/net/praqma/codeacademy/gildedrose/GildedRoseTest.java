d6a59e9104bb36ee2c3fefc7c89cfed89b67a09e
luciosis <marc.henriksen90@gmail.com>
2018-08-22 10:06:36 +0200

    Fixed the shiet

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-***
-New test 
-***
+/*
+new comment
+*/
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
